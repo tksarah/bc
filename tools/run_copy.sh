8f3a05a9723998018cc1eb7fcd1b237d6baad27f
tksarah <tsarah8139@gmail.com>
2024-03-15 14:26:49 +0900

    update 03151426

--- tools/run_copy.sh
+++ tools/run_copy.sh
@@ -6,6 +6,9 @@ file="../store/data"
 # Save directory 
 SDIR="../store/save"
 
+# docs directory 
+DOCS="../docs"
+
 ### Run
 echo -n "Run script -> "
 perl ./script.pl
@@ -23,20 +26,22 @@ timestamp=$(date -r "$file" +"%Y%m%d%H%M%S")
 # Save file name
 sfile="${timestamp}-data.csv"
 
-### Copy (From WSL to Win11)
-echo -n "Run copy to Desktop -> "
+### Copy 
+echo -n "Run copy to Desktop ->" 
 cp $file.csv /mnt/c/Users/sarah/Desktop/
+echo -n "Run copy to DOCS -> "
+cp $file.csv $DOCS/
+echo -n "Run save data -> "
+cp -p $file.csv  $SDIR/$sfile
 echo "Done copy"
 
 ### Save org data file
-echo -n "Run save data -> "
-cp -p $file.csv  $SDIR/$sfile
+echo -n "CSV Data saving -> "
 (cd $SDIR; tar czf $sfile.tar.gz $sfile )
-touch $file
 echo "Done save"
 
 ### Cleanup
 echo -n "Run cleanup -> "
-rm $file $SDIR/$sfile chaindapps.csv chaindapps.json dappssimple.csv dappssimple.json api_data.csv after_voting.csv
+rm $file $file.csv $SDIR/$sfile chaindapps.csv chaindapps.json dappssimple.csv dappssimple.json api_data.csv after_voting.csv
 echo "Done cleanup"
 touch $file
