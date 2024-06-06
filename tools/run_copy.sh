516bd6a0bf743eab6df6bbad12d81a4d13810741
tksarah <tsarah8139@gmail.com>
2024-06-07 08:25:49 +0900

    Update 20240607

--- tools/run_copy.sh
+++ tools/run_copy.sh
@@ -54,6 +54,8 @@ echo "Done"
 
 ### Cleanup
 echo -n "Run cleanup -> "
-rm $file $file.csv $SDIR/$sfile chaindapps.csv chaindapps.json dappssimple.csv dappssimple.json api_data.csv after_voting.csv index.html
+#rm $file $file.csv $SDIR/$sfile chaindapps.csv chaindapps.json dappssimple.csv dappssimple.json api_data.csv after_voting.csv index.html
+# Leave api_data.csv
+rm $file $file.csv $SDIR/$sfile chaindapps.csv chaindapps.json dappssimple.csv dappssimple.json after_voting.csv index.html
 echo "Done"
 touch $file
