c2c4a49b24bab94269aa3bde2e6e51517fbb7240
tksarah <tsarah8139@gmail.com>
2025-07-04 15:37:21 +0900

    Update get_price.pl
    Add QUICK-new

--- tools/get_price.pl
+++ tools/get_price.pl
@@ -1,97 +1,72 @@
 #!/usr/bin/perl
-# https://coinmarketcap.com/api/documentation/v1/
 use strict;
 use warnings;
 use JSON;
 use LWP::Simple;
-use Data::Dumper;
 
 my $url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest';
-my $api_key = 'YOUR_API_KEY';
-my $headers = '-H "X-CMC_PRO_API_KEY: '.$api_key.'" -H "Accept: application/json"';
+my $api_key = 'YOUR-API-KEY';
 
-# for USD
-my $params_usd = '-d "symbol=ETH,ASTR,SOL,QUICK&convert=USD"';
-my $curl_command_usd = "curl -s $headers $params_usd -G \"$url\"";
+# トークンIDとその対応するsymbolを記述
+my %tokens = (
+    '1'  => 'BTC',
+    '1027'  => 'ETH',
+    '5426'  => 'SOL',
+    '12885' => 'ASTR',
+    '19966' => 'QUICK',
+);
 
-my $json_usd = `$curl_command_usd`;
-die "Could not get $url" unless defined $json_usd;
-
-my $decoded_json_usd = decode_json($json_usd);
-
-# TEST
-#print Dumper($decoded_json);
-
-my $eth_price_usd = $decoded_json_usd->{'data'}{'ETH'}{'quote'}{'USD'}{'price'};
-my $astr_price_usd = $decoded_json_usd->{'data'}{'ASTR'}{'quote'}{'USD'}{'price'};
-my $sol_price_usd = $decoded_json_usd->{'data'}{'SOL'}{'quote'}{'USD'}{'price'};
-my $quick_price_usd = $decoded_json_usd->{'data'}{'QUICK'}{'quote'}{'USD'}{'price'};
-
-# for JPY
-my $params_jpy = '-d "symbol=ETH,ASTR,SOL,QUICK&convert=JPY"';
-my $curl_command_jpy = "curl -s $headers $params_jpy -G \"$url\"";
-
-my $json_jpy = `$curl_command_jpy`;
-die "Could not get $url" unless defined $json_jpy;
-
-my $decoded_json_jpy = decode_json($json_jpy);
-
-my $eth_price_jpy = $decoded_json_jpy->{'data'}{'ETH'}{'quote'}{'JPY'}{'price'};
-my $astr_price_jpy = $decoded_json_jpy->{'data'}{'ASTR'}{'quote'}{'JPY'}{'price'};
-my $sol_price_jpy = $decoded_json_jpy->{'data'}{'SOL'}{'quote'}{'JPY'}{'price'};
-my $quick_price_jpy = $decoded_json_jpy->{'data'}{'QUICK'}{'quote'}{'JPY'}{'price'};
-
-my $token = $ARGV[0] // 'default';
-
-# Output
-if($token eq "eth"){
-        print "\n--- Price ---\n";
-        print "\$/Yen:\n";
-        printf("  %.2f\n", $eth_price_jpy/$eth_price_usd);
-        print "ETH\n";
-        printf("  USD  : %.2f\n", $eth_price_usd);
-        printf("  JPY  : %.2f\n", $eth_price_jpy);
-        print "-------------\n\n";
-}elsif($token eq "astr"){
-        print "\n--- Price ---\n";
-        print "\$/Yen:\n";
-        printf("  %.2f\n", $eth_price_jpy/$eth_price_usd);
-        print "ASTR\n";
-        printf("  USD : %.4f\n", $astr_price_usd);
-        printf("  JPY : %.2f\n", $astr_price_jpy);
-        print "-------------\n\n";
-}elsif($token eq "sol"){
-        print "\n--- Price ---\n";
-        print "\$/Yen:\n";
-        printf("  %.2f\n", $sol_price_jpy/$sol_price_usd);
-        print "SOL\n";
-        printf("  USD : %.4f\n", $sol_price_usd);
-        printf("  JPY : %.2f\n", $sol_price_jpy);
-        print "-------------\n\n";
-}elsif($token eq "quick"){
-        print "\n--- Price ---\n";
-        print "\$/Yen:\n";
-        printf("  %.2f\n", $quick_price_jpy/$quick_price_usd);
-        print "QUICK\n";
-        printf("  USD : %.4f\n", $quick_price_usd);
-        printf("  JPY : %.2f\n", $quick_price_jpy);
-        print "-------------\n\n";
-}else{
-        print "\n--- Price ---\n";
-        print "\$/Yen:\n";
-        printf("  %.2f\n\n", $eth_price_jpy/$eth_price_usd);
-        print "USD:\n";
-        printf("  ETH  : %.2f\n", $eth_price_usd);
-        printf("  SOL  : %.4f\n", $sol_price_usd);
-        printf("  QUICK  : %.4f\n", $quick_price_usd);
-        printf("  ASTR : %.4f\n", $astr_price_usd);
-        print "\n";
-        print "JPY:\n";
-        printf("  ETH  : %.2f\n", $eth_price_jpy);
-        printf("  SOL  : %.2f\n", $sol_price_jpy);
-        printf("  QUICK  : %.2f\n", $quick_price_jpy);
-        printf("  ASTR : %.2f\n", $astr_price_jpy);
-        print "-------------\n\n";
+sub fetch_prices {
+    my ($convert) = @_;
+    my $id_str = join(',', keys %tokens);
+    my $headers = '-H "X-CMC_PRO_API_KEY: ' . $api_key . '" -H "Accept: application/json"';
+    my $params = qq{-d "id=$id_str&convert=$convert"};
+    my $command = qq{curl -s $headers $params -G "$url"};
+    my $json = `$command`;
+    die "Could not get data for $convert" unless $json;
+    return decode_json($json);
 }
 
+sub extract_prices {
+    my ($json, $currency) = @_;
+    my %prices;
+    for my $id (keys %tokens) {
+        my $symbol = $tokens{$id};
+        $prices{$symbol} = $json->{data}{$id}{quote}{$currency}{price};
+    }
+    return \%prices;
+}
+
+sub print_price_info {
+    my ($usd_prices, $jpy_prices, $target) = @_;
+    print "\n--- Price ---\n";
+    my $rate = $jpy_prices->{ETH} / $usd_prices->{ETH};
+    printf("\$/Yen:\n  %.2f\n", $rate);
+
+    if ($target && exists $usd_prices->{$target}) {
+        print "$target\n";
+        printf("  USD : %.4f\n", $usd_prices->{$target});
+        printf("  JPY : %.2f\n", $jpy_prices->{$target});
+    } else {
+        print "\nUSD:\n";
+        for my $symbol (sort keys %$usd_prices) {
+            printf("  %-6s : %.4f\n", $symbol, $usd_prices->{$symbol});
+        }
+        print "\nJPY:\n";
+        for my $symbol (sort keys %$jpy_prices) {
+            printf("  %-6s : %.2f\n", $symbol, $jpy_prices->{$symbol});
+        }
+    }
+    print "-------------\n\n";
+}
+
+
+my $token = uc($ARGV[0] // '');
+my $json_usd = fetch_prices('USD');
+my $json_jpy = fetch_prices('JPY');
+
+my $usd_prices = extract_prices($json_usd, 'USD');
+my $jpy_prices = extract_prices($json_jpy, 'JPY');
+
+print_price_info($usd_prices, $jpy_prices, $token);
 exit(0);
